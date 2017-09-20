1a6e534ec5e190daabb4b512d5736c814b8a84df
Mark Goho <markgoho@gmail.com>
2017-09-20 11:04:40 -0400

    Adding javascript for functionality

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
-const bodyChildren = document.body.childNodes;
-
-for (const child of bodyChildren) {
-  console.log(child);
+function playSound(e) {
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+  if (!audio) return; // stop the function if no match is found
+  audio.currentTime = 0; // rewind to start
+  audio.play();
+  key.classList.add('playing');
 }
+
+function removeTransition(e) {
+  if (e.propertyName !== 'transform') return; // skip if transition isn't transform
+  this.classList.remove('playing');
+}
+
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+
+window.addEventListener('keydown', playSound);
